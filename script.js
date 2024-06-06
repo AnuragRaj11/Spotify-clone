fd2b6151f8c0e930c43f3bd9863ec5d8ab5bbae5
AnuragRaj11 <Araj59519@gmail.com>
2024-06-06 14:53:44 +0530

    JavaScript functionality added

--- script.js
+++ script.js
@@ -1,33 +1,74 @@
-let currentsong=new Audio();
+console.log('Lets write JavaScript');
+let currentSong = new Audio();
 let songs;
-let currentfolder;
+let currFolder;
 
-function secondstoMinutesSeconds(seconds) {
-    if (isNaN(seconds) || seconds<0) {
+function secondsToMinutesSeconds(seconds) {
+    if (isNaN(seconds) || seconds < 0) {
         return "00:00";
     }
 
-    const min=Math.floor(seconds/60);
-    const remaingsec=Math.floor(seconds%60);
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
 
-    const formatmin=String(min).padStart(2,'0');
-    const formatsec=String(remaingsec).padStart(2,'0');
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
 
-    return '${formatmin}:${formatsec}';
+    return `${formattedMinutes}:${formattedSeconds}`;
 }
 
-async function getsongs(folder) {
-    currentfolder=folder;
-    let a=await fetch('/${folder}/')
-    let resp=await a.text();
-    let div=document.createElement("div")
-    div.innerHTML=resp;
-    let as=div.getElementsByTagName("a")
-    songs=[]
-    for (let index = 0; index < as.length.length; index++) {
+async function getSongs(folder) {
+    currFolder = folder;
+    let a = await fetch(`/${folder}/`)
+    let response = await a.text();
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    songs = []
+    for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split('/${folder}/')[1])
+            songs.push(element.href.split(`/${folder}/`)[1])
         }
     }
+
+
+
+
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    songUL.innerHTML = ""
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" width="34" src="imgs/music.svg" alt="">
+                            <div class="info">
+                                <div> ${song.replaceAll("%20", " ")}</div>
+                                <div>AR</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img class="invert" src="imgs/play.svg" alt="">
+                            </div> </li>`;
+    }
+
+
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+
+        })
+    })
+
+    return songs
 }
+const playMusic = (track, pause = false) => {
+    currentSong.src = `/${currFolder}/` + track
+    if (!pause) {
+        currentSong.play()
+        play.src = "imgs/pause.svg"
+    }
+    document.querySelector(".songinfo").innerHTML = decodeURI(track)
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
+
+
+}
+
+main()
